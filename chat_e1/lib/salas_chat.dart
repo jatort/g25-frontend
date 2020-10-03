3786c4cafe1c76a30482aed88bf3e54f0c25f69a
jatort <jatort@uc.cl>
2020-10-03 18:03:36 -0300

    realtime create chatroom prueba 2 pcs

--- chat_e1/lib/salas_chat.dart
+++ chat_e1/lib/salas_chat.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-
+import 'package:scoped_model/scoped_model.dart';
+import './rooms_model.dart';
 import 'chat.dart';
 
 ListViewHandelItemState chatsPage;
@@ -16,6 +17,12 @@ class ListViewHandelItem extends StatefulWidget {
 
 /// https://here4you.tistory.com/185
 class ListViewHandelItemState extends State<ListViewHandelItem> {
+  @override
+  void initState() {
+    super.initState();
+    ScopedModel.of<RoomsModel>(context, rebuildOnChange: false).init();
+  }
+
   List<String> items = <String>['Sala 1', 'Games', 'Music'];
 
   final nameChatController = TextEditingController(
@@ -32,54 +39,76 @@ class ListViewHandelItemState extends State<ListViewHandelItem> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Chat List")),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
-                return Dismissible(
-                  key: Key(item),
-                  direction: DismissDirection.startToEnd,
-                  child: ListTile(
-                    title: Text(item),
-                    trailing: IconButton(
-                      icon: Icon(Icons.send),
-                      color: Colors.blue,
-                      onPressed: () {
-                        /// Da instrucciones de qué hacer cuando se presiona el botón
-                        Route route =
-                            MaterialPageRoute(builder: (bc) => Chat(item));
+      body: ScopedModelDescendant<RoomsModel>(
+        builder: (context, child, model) {
+          return Column(
+            children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                  itemCount: model.roomList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    final item = items[index];
+                    return Dismissible(
+                      key: Key(item),
+                      direction: DismissDirection.startToEnd,
+                      child: ListTile(
+                        title: Text(item),
+                        trailing: IconButton(
+                          icon: Icon(Icons.send),
+                          color: Colors.blue,
+                          onPressed: () {
+                            /// Da instrucciones de qué hacer cuando se presiona el botón
+                            Route route =
+                                MaterialPageRoute(builder: (bc) => Chat(item));
 
-                        /// Acá redirecciona al Chat
-                        Navigator.of(context).push(route);
+                            /// Acá redirecciona al Chat
+                            Navigator.of(context).push(route);
 
-                        /// pushea la ruta del Chat
-                      },
+                            /// pushea la ruta del Chat
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Divider(
+                color: Colors.blue,
+                height: 5,
+                indent: 10,
+                endIndent: 10,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Row(
+                  children: <Widget>[
+                    Text("Chat Name:"),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: TextField(
+                          controller: nameChatController,
+                          onSubmitted: (text) {
+                            setState(() {
+                              if (nameChatController.text != "") {
+                                items.add(nameChatController.text);
+                              }
+                            });
+                            nameChatController.clear();
+                          },
+                        ),
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
-          ),
-          Divider(
-            color: Colors.blue,
-            height: 5,
-            indent: 10,
-            endIndent: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Row(
-              children: <Widget>[
-                Text("Chat Name:"),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: TextField(
-                      controller: nameChatController,
-                      onSubmitted: (text) {
+                    RaisedButton(
+                      child: new Text(
+                        "New Chat Room",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      color: Colors.green,
+                      onPressed: () {
                         setState(() {
                           if (nameChatController.text != "") {
                             items.add(nameChatController.text);
@@ -87,30 +116,13 @@ class ListViewHandelItemState extends State<ListViewHandelItem> {
                         });
                         nameChatController.clear();
                       },
-                    ),
-                  ),
+                    )
+                  ],
                 ),
-                RaisedButton(
-                  child: new Text(
-                    "New Chat Room",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                  color: Colors.green,
-                  onPressed: () {
-                    setState(() {
-                      if (nameChatController.text != "") {
-                        items.add(nameChatController.text);
-                      }
-                    });
-                    nameChatController.clear();
-                  },
-                )
-              ],
-            ),
-          ),
-        ],
+              ),
+            ],
+          );
+        },
       ),
     );
   }
