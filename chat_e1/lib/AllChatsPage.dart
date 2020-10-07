25f2c7b6638b0a2988225e6d156def28a419c4b9
jatort <jatort@uc.cl>
2020-10-06 21:14:44 -0300

    no puedo wrappear lista de chats junto con form

--- chat_e1/lib/AllChatsPage.dart
+++ chat_e1/lib/AllChatsPage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
-
 import './ChatPage.dart';
 import './ChatRoom.dart';
 import './ChatModel.dart';
@@ -30,13 +29,14 @@ class _AllChatsPageState extends State<AllChatsPage> {
   Widget buildAllChatList() {
     return ScopedModelDescendant<ChatModel>(
       builder: (context, child, model) {
+        List<ChatRoom> chatrooms = model.getChatRooms();
         return ListView.builder(
-          itemCount: model.chatRoomList.length,
+          itemCount: chatrooms.length,
           itemBuilder: (BuildContext context, int index) {
-            ChatRoom chatroom = model.chatRoomList[index];
+            // ChatRoom chatroom = model.chatRoomList[index];
             return ListTile(
-              title: Text(chatroom.name),
-              onTap: () => chatRoomClicked(chatroom),
+              title: Text(chatrooms[index].name),
+              onTap: () => chatRoomClicked(chatrooms[index]),
             );
           },
         );
